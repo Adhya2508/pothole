c758617025f6e3c90149e2e99c28fca33e10cbcd
Adhya Sharma <adhyasharma2005@gmail.com>
2026-06-29 23:53:31 +0530

    Change data types for prediction fields in history.py
    Updated data types for prediction fields to ensure proper formatting when storing in DynamoDB.

--- backend/history.py
+++ backend/history.py
@@ -1,6 +1,7 @@
 import boto3
 import uuid
 from datetime import datetime
+from decimal import Decimal
 
 # Connect to DynamoDB
 dynamodb = boto3.resource("dynamodb")
@@ -12,12 +13,12 @@ def save_prediction(data):
     prediction = {
         "prediction_id": str(uuid.uuid4()),
         "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-        "pothole_count": data["pothole_count"],
-        "average_confidence": data["average_confidence"],
-        "severity": data["severity"],
-        "covered_area_percent": data["covered_area_percent"],
-        "annotated_image": data["annotated_image"],
-        "annotated_image_url": data["annotated_image_url"]
+        "pothole_count": int(data["pothole_count"]),
+        "average_confidence": Decimal(str(data["average_confidence"])),
+        "severity": str(data["severity"]),
+        "covered_area_percent": Decimal(str(data["covered_area_percent"])),
+        "annotated_image": str(data["annotated_image"]),
+        "annotated_image_url": str(data["annotated_image_url"])
     }
 
     table.put_item(Item=prediction)
